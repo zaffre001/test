27a77ec975066852b90128e6a26e22a9fa36d83e
zaffre <zaffre@zaffre-mac.local>
2020-11-22 15:55:18 +0900

    first commit

---
+++ all.js
@@ -0,0 +1,19 @@
+
+// function start() {
+//     content = document.getElementById("content");
+//     question = document.getElementById("question");
+
+//     question.innerText = "테스트를 시작할 준비가 되셨나요?";
+//     makeButton("네~!", ()=>{});
+// }
+
+// function makeButton(text, callback){
+//     let btn = document.createElement("button");
+//     btn.innerText = text;
+//     btn.addEventListener("click", callback);
+//     content.appendChild(btn);
+// }
+
+function onButtonClick(){
+    alert("dsdsad");
+}
